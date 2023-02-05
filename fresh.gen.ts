419e9afcee1176aea5b2a5152858c97ca7fdfcce
MateuszOwczarek <mateuszowczarek@onet.eu>
2023-02-05 17:55:40 +0100

    updated import maps

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,22 +4,32 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/avatars.tsx";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/name/[name].tsx";
+import * as $1 from "./routes/_middleware.ts";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/api/login.ts";
+import * as $4 from "./routes/avatars.tsx";
+import * as $5 from "./routes/dashboard/_middleware.ts";
+import * as $6 from "./routes/dashboard/index.tsx";
+import * as $7 from "./routes/index.tsx";
+import * as $8 from "./routes/name/[name].tsx";
 import * as $$0 from "./islands/Counter.tsx";
+import * as $$1 from "./islands/MainAlert.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/avatars.tsx": $2,
-    "./routes/index.tsx": $3,
-    "./routes/name/[name].tsx": $4,
+    "./routes/_middleware.ts": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/api/login.ts": $3,
+    "./routes/avatars.tsx": $4,
+    "./routes/dashboard/_middleware.ts": $5,
+    "./routes/dashboard/index.tsx": $6,
+    "./routes/index.tsx": $7,
+    "./routes/name/[name].tsx": $8,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
+    "./islands/MainAlert.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,
