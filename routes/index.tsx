521c11ffd085954a1920b787bb3ebe91fe6451cc
MateuszOwczarek <mateuszowczarek@onet.eu>
2023-02-05 17:56:53 +0100

    added main page alert message

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,44 @@
-import Layout from "../components/Layout.tsx";
-import Counter from "../islands/Counter.tsx";
+import { Handlers } from "$fresh/server.ts";
+import Layout from "@/components/Layout.tsx";
+import Counter from "@/islands/Counter.tsx";
+import { isLogged } from "@/signals/isLogged.tsx";
+import MainAlert from "../islands/MainAlert.tsx";
+import envConfig from "../utils/config.ts";
 
-export default function Home() {
+export const handler: Handlers = {
+ 	GET(req, ctx) {
+		if(ctx.state.user){
+			isLogged.value = true;
+			console.log(ctx.state.user, isLogged.value)
+		}else {
+			isLogged.value = false;
+		}
+		const params = new URLSearchParams(req.url.split("?")[1]);
+		const message = params.get("message");
+		if(message){
+			console.log(message)
+			return ctx.render({
+				message: params.get("message"),
+			});
+		}
+		else {
+			return ctx.render({})
+		}
+  },
+};
+
+interface HomeParamsInterface {
+	message?: string
+}
+
+export default function Home({data}: {data: HomeParamsInterface}) {
   return (
      <Layout title="Home page test">
 			<>
-			Twoja stara
-        <p>
+			{data.message && <MainAlert message={data.message}/>}
+			<h1>Hello</h1>
+        {isLogged.value === true ? <p>You are logged. Go to <a href={envConfig.base_url + "/dashboard"}>dashboard.</a></p> : 'Go ahead and log in'}
+				<p>
           Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
           file, and refresh.
         </p>
