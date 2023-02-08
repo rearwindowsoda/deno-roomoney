77cb4f7a92a33d537ab1a67ac2b2049ed430c354
MateuszOwczarek <mateuszowczarek@onet.eu>
2023-02-08 19:15:24 +0100

    updated user interface

--- interfaces/UserInterface.ts
+++ interfaces/UserInterface.ts
@@ -2,8 +2,8 @@ import { Types } from "npm:mongoose@^6.7";
 import { z } from "zod";
 
 const UserWithoutId = z.object({
-  login: z.string({ required_error: "Login should be at least 5 characters long but not more than 10." }).min(5).max(10),
-  password: z.string({ required_error: "Password should be at least 5 characters long but not more than 15." }).min(5).max(15),
+  login: z.string({ required_error: "Login should be at least 5 characters long but not more than 20." }).min(5).max(20),
+  password: z.string({ required_error: "Password should be at least 5 characters long but not more than 20." }).min(5).max(20),
   createdAt: z.date({ required_error: "Date is required" }).default(new Date()),
   isAdmin: z.boolean().default(false),
   houses: z.string().array().default([]),
